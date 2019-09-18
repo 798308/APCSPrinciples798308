27fd77c4c70bc60f26592e7b7db43d7674c22a4a
798308 <54591150+798308@users.noreply.github.com>
2019-09-18 08:51:07 -0700

    School Updates to Paddle Gaame

--- Projects/Paddle Game Project/sketch.js
+++ Projects/Paddle Game Project/sketch.js
@@ -19,6 +19,7 @@ function setup() {
   cnv.position((windowWidth-width)/2, 30);
   background(20, 20, 20);
   loadObjects(10);
+
 }
 
 //title screen
@@ -27,58 +28,43 @@ function startGame(){
   textSize(90);
   fill(20,20,20);
   text("Paddle Game",150,300);
-  for (var i = 0; i < 4; i++){
-    if (i === 0){
-      fill(1, 255, 1);
-    }
-    if (i === 1){
-      fill(255, 255, 1);
-    }
-    if (i === 2){
-      fill(255, 1, 1);
-    }
-    if (i === 3){
-      fill(1, 1, 255);
-    }
-    rect((i * 200) + 50, 500, 100, 75, 20);
-  }
-  for (var n = 0; n < 4; n++){
-    fill(20,20,20);
+  for (var i = 0; i < 4; i++){//buttons and labels
     textFont('Georgia');
     textSize(18);
     var label;
-    if (n === 0){
+    if (i === 0){
+      fill(1, 255, 1);
       label = "Easy";
     }
-    if (n === 1){
+    if (i === 1){
+      fill(255, 255, 1);
       label = "Medium";
     }
-    if (n === 2){
+    if (i === 2){
+      fill(255, 1, 1);
       label = "Hard";
     }
-    if (n === 3){
+    if (i === 3){
+      fill(1, 1, 255);
       label = "Instructions";
     }
-    text(str(label), (n * 200) + 55, 550);
+    rect((i * 200) + 50, 500, 100, 75, 20);
+    fill(20,20,20);
+    text(str(label), (i * 200) + 55, 550);
+  }
+  if (mouseX > 50 && mouseX < 150 && mouseY > 500 && mouseY < 575){
+    gameState = 2;
+    console.log(gameState);
   }
 }
+
+
 
 //game code
 function playGame(){
   function draw(){
     background(20,20,20,50);
     runObjects();
-
-  }
-
-  function runObjects(){
-    for(var i = 0; i < balls.length; i++){
-      balls[i].run();
-      if (balls[i] === true){
-        balls[i].splice();
-      }
-    }
-    paddle.run();
   }
 }
 
@@ -86,10 +72,22 @@ function playGame(){
 //displaying game states
 function draw() {
   if (gameState === 1){
-    startGame();
+    playGame();
   }
   if (gameState === 2){
     playGame();
   }
+}
+function loadObjects(n){
+  for (var i = 0; i < n; i++){
+    balls[i] = new Ball(random(width), random(0,200), random(-3,3), random(-3,3));
+  }
+  paddle = new Paddle(width/2, 700);
+}
 
+function runObjects(){
+  for(var i = 0; i < balls.length; i++){
+    balls[i].run();
+  }
+  paddle.run();
 }
