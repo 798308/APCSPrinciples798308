78a0017b216f0c5955033da00ae6a19826d07665
798308 <54591150+798308@users.noreply.github.com>
2019-10-04 08:50:45 -0700

    Sort

---
+++ Labs/Lab 104 My own Sorting Code/sketch.js
@@ -0,0 +1,24 @@
+//  Your Name
+// 	Date or version number
+//  This is a comment
+//  The setup function function is called once when your program begins
+function setup() {
+  fill(200, 30, 150);
+  var list = [3,6,1,8,2,9];
+  function swap(list,a,b){
+    var temp = list[a];
+    list[a] = list[b];
+    list[b] = temp;
+  }
+for(i = 0; i < list.length; i++){
+  
+}
+
+
+
+}
+
+//  The draw function is called @ 30 fps
+function draw() {
+
+}
