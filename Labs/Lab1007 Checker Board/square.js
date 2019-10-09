7783d7f91fc1234757972874dd2d1c3b46369d59
798308 <54591150+798308@users.noreply.github.com>
2019-10-09 08:41:38 -0700

    changing the board color

--- Labs/Lab1007 Checker Board/square.js
+++ Labs/Lab1007 Checker Board/square.js
@@ -10,19 +10,18 @@ class Square{
     this.render();
   }
   render(){
-    noStroke()
     if(rowNum % 2 === 0){
       if(number % 2 === 0){
-        fill(255,255,255);
+        fill(250,245,230);
       }else{
-        fill(0);
+        fill(255,228,196);
       }
     }
     if(rowNum % 2 === 1){
       if(number % 2 === 0){
-        fill(0);
+        fill(255,228,196);
       }else{
-        fill(255, 255, 255);
+        fill(250,245,230);
       }
     }
     rect(this.x, this.y, this.w, this.w);
