fa65baab7ee399e73a45cf1657f8ef573ce48934
798308 <54591150+798308@users.noreply.github.com>
2019-10-11 08:50:42 -0700

    deleted everything on accident :(

--- Labs/Lab1004 My own Sorting Code/sketch.js
+++ Labs/Lab1004 My own Sorting Code/sketch.js
@@ -2,27 +2,17 @@
 // 	Date or version number
 //  This is a comment
 //  The setup function function is called once when your program begins
+var list = [3,7,2,9,10,1];
 function setup() {
-  var list = [3,6,1,8,2,9];
-  function swap(list,a,b){
-    var temp = list[a];
-    list[a] = list[b];
-    list[b] = temp;
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(5, 5, 5);
+  fill(200, 30, 150);
+  function swap(){
+    
   }
-  console.log(list);
-  for(var n = 0; n < list.length; n++){
-    for(var i = list.length; i > 0; i--){
-      a = 0;
-      a = a + 1;
-      if(list[0] > list[i]){
-        swap(list,0,i);
-      }
-      if(list[a] > list[i]){
-        swap(list,i,i+1);
-      }
-    }
-  }
-  console.log(list);
+
+
 
 }
 
