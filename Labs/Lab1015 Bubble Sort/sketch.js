4ff65354996f3da3a5f1f2564424efebde5bb6da
798308 <54591150+798308@users.noreply.github.com>
2019-10-16 21:08:06 -0700

    Finished bubble sort

--- Labs/Lab1015 Bubble Sort/sketch.js
+++ Labs/Lab1015 Bubble Sort/sketch.js
@@ -1,13 +1,19 @@
 // 	Date or version number
 //  This is a comment
 //  The setup function function is called once when your program begins
-var list = [3,7,2,9,10,1];
+var list = [];
 function setup() {
   function swap(list,a,b){
     var temp = list[a];
     list[a] = list[b];
     list[b] = temp;
   }
+  function makeList(n,list){
+    for (var i = 0; i < n; i++){
+      list[i] = int(random(0, n));
+    }
+  }
+  makeList(2500,list);
   for(var i = 0; i < list.length - 1; i++){
     for (j = 0; j < list.length - 1 - i; j++){
       if(list[j] > list[j + 1]){
@@ -15,10 +21,5 @@ function setup() {
       }
     }
   }
-}
-console.log(list);
-function makeList(n,list){
-  for (var i = 0; i <= n; i++){
-    list[i] = random(0, n);
-  }
+  console.log(list);
 }
