f1a0a3e60689ab6020f8f540d893a609b7883ca0
798308 <54591150+798308@users.noreply.github.com>
2019-10-21 08:50:42 -0700

    Selection Sort

--- Labs/Lab1018 Selection Sort/sketch.js
+++ Labs/Lab1018 Selection Sort/sketch.js
@@ -1,8 +1,8 @@
-// 	Date or version number
+// Austin Matel
+// 	10/18/19
 //  This is a comment
 //  The setup function function is called once when your program begins
 var list = [];
-var arr = [];
 function setup() {
   function swap(list,a,b){
     var temp = list[a];
@@ -14,16 +14,18 @@ function setup() {
       list[i] = int(random(0, n));
     }
   }
-  makeList(5,list);
+  makeList(200,list);
   console.log(list);
-  for(var i = 0; i === list.length; i++){
-    i--;
-    for (j = 0; j > list.lenght; j++){
-      if(list[j] < list[i]){
-        swap(list, j, i);
-        append(arr, list[i]);
+  for(var i = 0; i < list.length - 1; i++){
+    var index = i;
+    for (var j = i + 1; j < list.length; j++){
+      if(list[j] < list[index]){
+        index = j;
         }
       }
+      var smallerNumber = list[index];
+      list[index] = list[i];
+      list[i] = smallerNumber;
     }
-    console.log(arr);
+    console.log(list);
   }
